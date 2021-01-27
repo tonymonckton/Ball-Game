536c6f4b567b4af6a77892ce38c1dad62b313c28
TonyMonckton <tony@tonymonckton.co.uk>
2021-01-27 21:02:06 +0000

    update camera vars

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: add distance and incline, and follow player
+
 public class CameraController : MonoBehaviour
 {
+    public float incline;
+    public float distance;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
